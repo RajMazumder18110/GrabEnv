21db977c3c2599ba8e1948e52eecce92d633673f
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2024-11-14 12:33:50 +0530

    feat: add main codebase with environment variable management functionality

---
+++ src/@types/environment.ts
@@ -0,0 +1,22 @@
+type CastType<Type> = Exclude<Type, undefined> extends string
+  ? "string"
+  : Exclude<Type, undefined> extends number
+  ? "number"
+  : Exclude<Type, undefined> extends boolean
+  ? "boolean"
+  : never;
+
+export type OutputVariables<T> = {
+  [K in keyof T]-?: T[K];
+} & {
+  NODE_ENV: "development" | "production";
+};
+
+export type Configs<T> = {
+  [K in keyof T]-?: undefined extends T[K]
+    ? {
+        type: CastType<T[K]>;
+        defaultValue: Exclude<T[K], undefined>;
+      }
+    : { type: CastType<T[K]> };
+};
---
+++ src/environment.ts
@@ -0,0 +1,56 @@
+/** @notice library imports */
+import "dotenv/config";
+import { z, ZodError, type ZodTypeAny } from "zod";
+/// Local imports
+import type { Configs, OutputVariables } from "./@types/environment";
+
+export function grabEnvironments<T>(configs: Configs<T>): OutputVariables<T> {
+  // Build the Zod schema dynamically based on the provided variable configurations
+  const schema = Object.keys(configs).reduce((prevSchema, variableKey) => {
+    let zodType: ZodTypeAny;
+    const variable = configs[variableKey as keyof typeof configs];
+
+    // Dynamically select the correct Zod type based on the variable's `type`
+    switch (variable.type) {
+      case "string":
+        zodType = z.string();
+        break;
+      case "number":
+        zodType = z.coerce.number();
+        break;
+      case "boolean":
+        zodType = z.coerce.boolean();
+        break;
+      default:
+        throw new Error(`[UNSUPPORTED_TYPE]: ${variable.type}`);
+    }
+
+    // Apply default value if provided
+    if ("defaultValue" in variable) {
+      zodType = zodType.default(variable.defaultValue);
+    }
+
+    // Attach the current validator to the schema
+    prevSchema[variableKey] = zodType;
+    return prevSchema;
+  }, {} as Record<string, ZodTypeAny>);
+
+  /// Include NODE_ENV
+  const nodeEnvIncludedSchema = z.object({
+    ...schema,
+    NODE_ENV: z.enum(["development", "production"]).default("development"),
+  });
+
+  try {
+    // Parse and return the validated environment variables with the inferred type
+    return nodeEnvIncludedSchema.parse(process.env) as OutputVariables<T>;
+  } catch (error) {
+    if (error instanceof ZodError) {
+      console.error(
+        "Environment variable validation error:",
+        error.flatten().fieldErrors
+      );
+    }
+    process.exit(1);
+  }
+}
